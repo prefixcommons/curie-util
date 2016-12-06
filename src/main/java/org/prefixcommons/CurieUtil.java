f3894e0f213e7961f12a6b54445df844201d84e9
Jeremy Nguyen Xuan <jnguyenxuan@lbl.gov>
2016-12-06 14:14:05 -0800

    first commit

---
+++ src/main/java/org/prefixcommons/CurieUtil.java
@@ -0,0 +1,123 @@
+package org.prefixcommons;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Collection;
+import java.util.Map;
+
+import org.prefixcommons.trie.Trie;
+
+import com.github.jsonldjava.core.Context;
+import com.github.jsonldjava.utils.JsonUtils;
+import com.google.common.base.Optional;
+import com.google.common.collect.ImmutableBiMap;
+
+
+public class CurieUtil {
+  final private Trie trie;
+  private final ImmutableBiMap<String, String> curieMap;
+
+  public CurieUtil(Map<String, String> mapping) {
+    Collection<String> iris = mapping.values();
+    this.curieMap = ImmutableBiMap.copyOf(mapping);
+    this.trie = new Trie();
+    for (String key : iris) {
+      this.trie.insert(key);
+    }
+  }
+
+  static public CurieUtil fromJsonLdFile(String filePath) throws IOException {
+    InputStream inputStream = new FileInputStream(filePath);
+    Object jsonObject = JsonUtils.fromInputStream(inputStream);
+    Context context = parseContext(jsonObject);
+    return new CurieUtil(context.getPrefixes(false));
+  }
+
+  /**
+   * Load a map of prefixes from the "@context" of a JSON-LD string.
+   *
+   * @param jsonString the JSON-LD string
+   * @return a map from prefix name strings to prefix IRI strings
+   * @throws IOException on any problem
+   */
+  private static Context parseContext(Object jsonObject) throws IOException {
+    try {
+      if (!(jsonObject instanceof Map)) {
+        return null;
+      }
+      Map<String, Object> jsonMap = (Map<String, Object>) jsonObject;
+      if (!jsonMap.containsKey("@context")) {
+        return null;
+      }
+      Object jsonContext = jsonMap.get("@context");
+      return new Context().parse(jsonContext);
+    } catch (Exception e) {
+      throw new IOException(e);
+    }
+  }
+
+  /***
+   * @return all of the CURIE prefixes
+   */
+  public Collection<String> getPrefixes() {
+    return curieMap.keySet();
+  }
+
+  /***
+   * Expand a CURIE prefix to the corresponding IRI prefix.
+   * 
+   * @param curiePrefix
+   * @return mapped IRI prefix
+   */
+  public String getExpansion(String curiePrefix) {
+    return curieMap.get(curiePrefix);
+  }
+
+  /***
+   * Returns the CURIE of an IRI, if mapped.
+   * 
+   * @param iri
+   * @return An {@link Optional} CURIE
+   */
+  public Optional<String> getCurie(String iri) {
+    String prefix = trie.getMatchingPrefix(iri);
+    if (prefix.equals("")) {
+      return Optional.absent();
+    } else {
+      String curiePrefix = curieMap.inverse().get(prefix);
+      return Optional
+          .of(String.format("%s:%s", curiePrefix, iri.substring(prefix.length(), iri.length())));
+    }
+  }
+
+  /***
+   * Expands a CURIE to a full IRI, if mapped.
+   * 
+   * @param curie
+   * @return an {@link Optional} IRI
+   */
+  public Optional<String> getIri(String curie) {
+    String[] parts = checkNotNull(curie).split(":");
+    if (parts.length > 1) {
+      String prefix = parts[0];
+      if (curieMap.containsKey(prefix)) {
+        return Optional.of(
+            String.format("%s%s", curieMap.get(prefix), curie.substring(curie.indexOf(':') + 1)));
+      }
+    }
+    return Optional.absent();
+  }
+
+  /***
+   * Returns the curie map.
+   * 
+   * @return the curie map
+   */
+  public Map<String, String> getCurieMap() {
+    return curieMap;
+  }
+
+}
---
+++ src/test/java/org/prefixcommons/CurieUtilTest.java
@@ -0,0 +1,91 @@
+package org.prefixcommons;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.hasItems;
+import static org.junit.Assert.assertThat;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import com.google.common.base.Optional;
+
+public class CurieUtilTest {
+
+  CurieUtil util;
+  Map<String, String> map = new HashMap<>();
+
+  @Before
+  public void setup() {
+    map.put("", "http://x.org/");
+    map.put("A", "http://x.org/a_");
+    map.put("B", "http://x.org/B_");
+    map.put("CC", "http://x.org/C_C");
+    map.put("C", "http://x.org/C_");
+    util = new CurieUtil(map);
+  }
+
+  @Test
+  public void curiePrefixes() {
+    assertThat(util.getPrefixes(), hasItems("A", "B"));
+  }
+
+  @Test
+  public void expansion() {
+    assertThat(util.getExpansion("A"), is("http://x.org/a_"));
+  }
+
+  @Test
+  public void absentIri_whenMappingIsNotPresent() {
+    assertThat(util.getIri("NONE:foo").isPresent(), is(false));
+  }
+
+  @Test
+  public void fullIri_whenInputHasNoPrefix() {
+    assertThat(util.getIri(":foo").get(), is("http://x.org/foo"));
+  }
+
+  @Test
+  public void curie_whenShortMappingIsPresent() {
+    assertThat(util.getCurie("http://x.org/foo"), is(Optional.of(":foo")));
+  }
+
+  @Test
+  public void curie_whenLongMappingIsPresent() {
+    assertThat(util.getCurie("http://x.org/a_foo"), is(Optional.of("A:foo")));
+  }
+
+  @Test
+  public void noCurie_whenMappingIsNotPresent() {
+    assertThat(util.getCurie("http://none.org/none"), is(Optional.<String>absent()));
+  }
+
+  @Test
+  public void curie_canBeDefinedInAnyOrder() {
+    assertThat(util.getCurie("http://x.org/C_Chello"), is(Optional.of("CC:hello")));
+  }
+
+  @Test
+  public void getMap() {
+    assertThat(util.getCurieMap(), is(map));
+  }
+
+  @Test
+  public void fromJsonLdFile() throws IOException {
+    CurieUtil curieUtil = CurieUtil.fromJsonLdFile("src/test/resources/obo_context.jsonld");
+    assertThat(curieUtil.getCurie("http://purl.obolibrary.org/obo/FIX_foo"),
+        is(Optional.of("FIX:foo")));
+    assertThat(curieUtil.getCurie("http://purl.obolibrary.org/obo/XAO_foo"),
+        is(Optional.of("XAO:foo")));
+    assertThat(curieUtil.getCurie("http://purl.obolibrary.org/none_foo"), is(Optional.absent()));
+    assertThat(curieUtil.getIri("NONE:foo").isPresent(), is(false));
+    assertThat(curieUtil.getIri("FIX:foo"),
+        is(Optional.of("http://purl.obolibrary.org/obo/FIX_foo")));
+    assertThat(curieUtil.getIri("XAO:foo"),
+        is(Optional.of("http://purl.obolibrary.org/obo/XAO_foo")));
+  }
+
+}
